a7d71b263126e45a7d12c3102e9ded3db80d29ae
david419kr <david419kr@gmail.com>
2026-01-01 18:42:44 +0900

    flutter design fix test2

--- android/flutter_app/lib/main.dart
+++ android/flutter_app/lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
         useMaterial3: true,
-        cardTheme: CardTheme(
+        cardTheme: CardThemeData(
           elevation: 2,
           shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
         ),
